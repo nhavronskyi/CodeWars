e2830ca468eb0378fcf2a9842dda590e9e15b9bc
nazar <nhavronskyi@gmail.com>
2023-04-28 07:02:21 +0200

    Nth Smallest Element

---
+++ src/main/java/codewars/seven/NthSmallestElement.java
@@ -0,0 +1,8 @@
+package codewars.seven;
+
+import java.util.Arrays;
+public class NthSmallestElement {
+    public static int nthSmallest(final int[] arr, final int n) {
+        return Arrays.stream(arr).sorted().toArray()[n-1];
+    }
+}
