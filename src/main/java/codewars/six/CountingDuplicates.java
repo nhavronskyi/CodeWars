963aa870f3a2538e035a46070751ec8777aec409
nazar <nhavronskyi@gmail.com>
2023-05-12 07:46:00 +0200

    CountingDuplicates

--- src/main/java/codewars/six/CountingDuplicates.java
+++ src/main/java/codewars/six/CountingDuplicates.java
@@ -1,9 +1,5 @@
 package codewars.six;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
