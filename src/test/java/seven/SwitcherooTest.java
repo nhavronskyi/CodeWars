342e05bb19d6bc0c8352b0e349ac61c554ff649f
nazar <nhavronskyi@gmail.com>
2023-05-16 07:26:58 +0200

    Build a square

---
+++ src/test/java/seven/SwitcherooTest.java
@@ -0,0 +1,15 @@
+package seven;
+
+import codewars.seven.Switcheroo;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class SwitcherooTest {
+    @Test
+    public void testSomething() {
+        assertEquals("abc", Switcheroo.switcheroo( "bac"));
+        assertEquals("ccc",Switcheroo.switcheroo("ccc"));
+        assertEquals("aaabcccbaaa",Switcheroo.switcheroo("bbbacccabbb"));
+    }
+}
