4546c7714ea9b0c893a27dae2e1ff237a8a633b3
nazar <nhavronskyi@gmail.com>
2023-05-17 08:17:01 +0200

    Scramblies

--- src/main/java/codewars/five/Scrambles.java
+++ src/main/java/codewars/five/Scrambles.java
@@ -10,7 +10,7 @@ public class Scrambles {
         Map<String, Long> str1Map = Stream.of(str1.split("")).collect(Collectors.groupingBy(s -> s, Collectors.counting()));
         Map<String, Long> str2Map = Stream.of(str2.split("")).collect(Collectors.groupingBy(s -> s, Collectors.counting()));
 
-        return (str2Map.entrySet().stream()
-                .allMatch(s -> s.getValue() <= Optional.ofNullable(str1Map.get(s.getKey())).orElse(-1L)));
+        return str2Map.entrySet().stream()
+                .allMatch(s -> s.getValue() <= Optional.ofNullable(str1Map.get(s.getKey())).orElse(-1L));
     }
 }
