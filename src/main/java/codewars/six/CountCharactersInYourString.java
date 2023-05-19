f4bb97ac82ae1b7c1ed3538166725f8cab6f3411
nazar <nhavronskyi@gmail.com>
2023-05-19 09:06:24 +0200

    Count characters in your string

--- src/main/java/codewars/six/CountCharactersInYourString.java
+++ src/main/java/codewars/six/CountCharactersInYourString.java
@@ -7,9 +7,6 @@ public class CountCharactersInYourString {
     public static Map<Character, Integer> count(String str) {
         return str.chars()
                 .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, v -> v.getValue().intValue()));
+                .collect(Collectors.groupingBy(c -> c, Collectors.summingInt(a -> 1)));
     }
 }
