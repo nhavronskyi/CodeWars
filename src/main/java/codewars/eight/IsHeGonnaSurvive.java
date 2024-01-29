a1c3300a20ed86eaec05dec8a76311b6a7944cbf
nhavronskyi <nhavronskyi@gmail.com>
2024-01-29 08:28:33 +0100

    Is he gonna survive?

---
+++ src/main/java/codewars/eight/IsHeGonnaSurvive.java
@@ -0,0 +1,8 @@
+package codewars.eight;
+
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+public class IsHeGonnaSurvive {
+    public static boolean hero(int bullets, int dragons) {
+        return bullets / 2 >= dragons;
+    }
+}
