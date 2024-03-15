2196c69d364ff13ec96511b3674c8ce7e9095aa5
nhavronskyi <nhavronskyi@gmail.com>
2024-03-15 22:30:11 +0100

    find_the_middle_element

---
+++ codewars-python/src/codewars/seven/find_the_middle_element.py
@@ -0,0 +1,4 @@
+def gimme(input_array):
+    sort = sorted(input_array)
+    m = sort[1]
+    return input_array.index(m)
---
+++ codewars-python/src/codewars/test/seven/find_the_middle_element_test.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+import src.codewars.seven.find_the_middle_element as task
+
+
+class MyTestCase(TestCase):
+    def test_something(self):
+        self.assertEqual(task.gimme([2, 3, 1]), 0, 'Finds the index of middle element')
+        self.assertEqual(task.gimme([5, 10, 14]), 1, 'Finds the index of middle element')
